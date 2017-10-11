1d3eb6f972881bc75ffd1236af6acb70815a47e9
PatrykBojar <patrickbojar12344@gmail.com>
2017-10-11 17:36:20 +0200

    Ejercicio2

--- ejerciciosTema1/ejerciciosFunciones/ex1.php
+++ ejerciciosTema1/ejerciciosFunciones/ex1.php
@@ -1,16 +1,19 @@
 <?php
 function tablaArray(){
+
 $noms = array ("nom"=> "Alberto Contador",
   "professio" => "Ciclista", "edat"=> 35,
   "naix"=> "Madrid", "equip"=>"Trek");
+$encontrada = "a";
 
 echo "<table border=1>";
   foreach ($noms as $k => $v) {
-    echo "<tr><td> $k </td> <td> $v </td> </tr>";
+    $pos = strpos($v, $encontrada);
+    if ($pos == true) {
+      echo "<tr><td> $k </td> <td> $v </td> </tr>";
+    }
   }
-
-echo "</table>";
-
+  echo "</table>";
 }
 
 tablaArray();
