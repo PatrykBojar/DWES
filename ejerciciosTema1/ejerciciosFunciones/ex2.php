a539a714fe552040d4fe5e51d4639ab685fae4dc
PatrykBojar <patrickbojar12344@gmail.com>
2017-10-11 17:39:32 +0200

    Separados los dos primeros ejercicios en archivos únicos

--- ejerciciosTema1/ejerciciosFunciones/ex2.php
+++ ejerciciosTema1/ejerciciosFunciones/ex2.php
@@ -4,10 +4,14 @@ function tablaArray(){
 $noms = array ("nom"=> "Alberto Contador",
   "professio" => "Ciclista", "edat"=> 35,
   "naix"=> "Madrid", "equip"=>"Trek");
+$encontrada = "a";
 
 echo "<table border=1>";
   foreach ($noms as $k => $v) {
+    $pos = strpos($v, $encontrada);
+    if ($pos == true) {
       echo "<tr><td> $k </td> <td> $v </td> </tr>";
+    }
   }
   echo "</table>";
 }
